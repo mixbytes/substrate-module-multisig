adf3382e27743e73205fd43c4bef498968e25f19
Eenae <z-line@my.com>
2018-11-29 16:07:24 +0300

    interface specified

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # substrate-module-multisig
-Experimental Substrate module for multisignature wallet management
+Experimental Substrate module for multisignature wallet management.
+
+**DO NOT** use it to store any funds or as a base for production code.
+
+At the moment it's just a playground.
+
---
+++ runtime/src/multisig.rs
@@ -0,0 +1,43 @@
+// Encoding library
+use parity_codec::Encode;
+
+// Enables access to the runtime storage
+use srml_support::{StorageValue, dispatch::Result};
+
+// Enables us to do hashing
+use runtime_primitives::traits::Hash;
+
+// Enables access to account balances
+use {balances, system::{self, ensure_signed}};
+
+
+pub trait Trait: balances::Trait + system::Trait {
+	/// The overarching event type.
+	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+}
+
+
+decl_module! {
+  pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+    fn deposit_event() = default;
+
+    // creates new multi-signature wallet
+    fn create(origin, owners: Vec<T::AccountId>, signatures_required: u8) -> Result {
+    }
+
+    // requests withdrawal from a wallet
+    // actual withdrawal will be made when there are enough signatures
+    fn withdraw(origin, wallet: T::AccountId, to: T::AccountId, value: T::Balance) -> Result {
+    }
+
+    // deposits are made using balances.transfer(to_wallet: T::AccountId, value: T::Balance)
+  }
+}
+
+
+decl_event!(
+	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
+		/// Created new wallet identified by Runtime::AccountId type.
+		Created(AccountId),
+	}
+);
